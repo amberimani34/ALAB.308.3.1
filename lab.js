63f1257f0eca4eddc89f7b7654ed9ae80ccecb63
Amber Roberts <a.roberts_90@yahoo.com>
2024-10-24 13:42:45 -0500

    part 2

--- lab.js
+++ lab.js
@@ -1,3 +1,5 @@
+
+console.log('Part 1');
 for (let i=1; i<=100; i++) {
     // console.log(i);
  if (i%3 === 0 && i%5 === 0) {
@@ -10,3 +12,12 @@ for (let i=1; i<=100; i++) {
     console.log(i);
 }
 }
+
+console.log('Part 2');
+let n = 50
+while (n >= 2) {
+    if (n === 2 || n === 3 || n === 5 || n === 7 || n === 11 || n === 13 || n === 17 || n === 19 || n === 23 || n === 29 || n === 31 || n === 37 || n === 41 || n === 43 || n === 47 || n === 53) {
+        console.log(n);
+    }   n--;
+}
+
