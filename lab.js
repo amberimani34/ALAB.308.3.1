38684ab7b48546953b061b366d1491fbaa09ffe0
Amber Roberts <a.roberts_90@yahoo.com>
2024-10-24 16:01:37 -0500

    part 3

--- lab.js
+++ lab.js
@@ -41,3 +41,56 @@ n++;
 
 
 console.log('Part 3')
+// const cell1 = 'ID, Name, Occupation, Age'
+// const cell2 = '42, Bruce, Knight, 41'
+// const cell3 = '57, Bob, Fry Cook, 19'
+// const cell4 = '63, Blaine, Quiz Master, 58'
+// const cell5 = "98, Bill, Doctor’s Assistant, 26"
+// console.log(cell1, cell2, cell3, cell4, cell5)
+
+let csvData = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+let cell = '';
+let cell1 = '';
+let cell2 = '';
+let cell3 = '';
+let cell4 = '';
+let cellNum = 1;
+
+for (let i = 0; i < csvData.length; i++) {
+    let char = csvData[i];
+
+    if (char == "," || char == "\n") {
+      switch (cellNum) {
+          case 1:
+              cell1 = cell;
+              break;
+          case 2:
+              cell2 = cell;
+              break;
+          case 3:
+              cell3 = cell;
+              break;
+          case 4:
+              cell4 = cell;
+              break;
+      }
+      if (cellNum == 4){
+          cellNum = 1;
+      }
+      else {
+          cellNum += 1;
+      }
+      cell = "";
+  } else {
+      cell += char;
+  }
+    if (char == "\n"){
+        console.log(cell1, cell2, cell3, cell4);
+        cell = "";
+        cell1 = '';
+        cell2 = '';
+        cell3 = '';
+        cell4 = '';
+    }
+}
